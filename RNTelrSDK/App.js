be29d32a7c5a33c2b394776382753b000ead4185
kondiramsonawane <111420253+kondiramsonawane@users.noreply.github.com>
2022-10-29 12:26:32 +0530

    project created

---
+++ RNTelrSDK/App.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.backgroundStyle}>
+     <Text>ttttttttt</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor:'white'
+  }
+});
+
+export default App;
